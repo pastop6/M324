a75491fd3d2b131ebae6ebcde015ebfe6b56a5fb
Markus <markus.lleshdedaj@edu.gbssg.ch>
2025-03-03 18:49:12 +0100

    feat: added Visual Elements

--- ToDoApp_UI/Form1.Designer.cs
+++ ToDoApp_UI/Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            listBoxTasks = new ListBox();
+            textBoxTask = new TextBox();
+            buttonAdd = new Button();
+            buttonRemove = new Button();
+            buttonSave = new Button();
+            SuspendLayout();
+            // 
+            // listBoxTasks
+            // 
+            listBoxTasks.FormattingEnabled = true;
+            listBoxTasks.ItemHeight = 15;
+            listBoxTasks.Location = new Point(324, 78);
+            listBoxTasks.Name = "listBoxTasks";
+            listBoxTasks.Size = new Size(120, 94);
+            listBoxTasks.TabIndex = 0;
+            // 
+            // textBoxTask
+            // 
+            textBoxTask.Location = new Point(136, 167);
+            textBoxTask.Name = "textBoxTask";
+            textBoxTask.Size = new Size(100, 23);
+            textBoxTask.TabIndex = 1;
+            // 
+            // buttonAdd
+            // 
+            buttonAdd.Location = new Point(380, 225);
+            buttonAdd.Name = "buttonAdd";
+            buttonAdd.Size = new Size(87, 23);
+            buttonAdd.TabIndex = 2;
+            buttonAdd.Text = "Hinzufügen";
+            buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // buttonRemove
+            // 
+            buttonRemove.Location = new Point(380, 254);
+            buttonRemove.Name = "buttonRemove";
+            buttonRemove.Size = new Size(87, 23);
+            buttonRemove.TabIndex = 3;
+            buttonRemove.Text = "Löschen";
+            buttonRemove.UseVisualStyleBackColor = true;
+            // 
+            // buttonSave
+            // 
+            buttonSave.Location = new Point(380, 283);
+            buttonSave.Name = "buttonSave";
+            buttonSave.Size = new Size(87, 23);
+            buttonSave.TabIndex = 4;
+            buttonSave.Text = "Speichern";
+            buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonSave);
+            Controls.Add(buttonRemove);
+            Controls.Add(buttonAdd);
+            Controls.Add(textBoxTask);
+            Controls.Add(listBoxTasks);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ListBox listBoxTasks;
+        private TextBox textBoxTask;
+        private Button buttonAdd;
+        private Button buttonRemove;
+        private Button buttonSave;
     }
 }
